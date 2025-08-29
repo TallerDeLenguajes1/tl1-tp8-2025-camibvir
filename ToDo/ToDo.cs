8ac7f001c9a1241b0f3669c6b3ee70d4b5c1e204
camibvir <virginiabaigorria005@gmail.com>
2025-08-29 19:05:18 -0300

    Update ToDo.cs

--- ToDo/ToDo.cs
+++ ToDo/ToDo.cs
@@ -5,7 +5,6 @@ namespace listaToDo
         public int TareaID { get; set; }
         public string? Descripcion { get; set; }
         public int Duracion { get; set; } // Validar que esté entre 10 y 100
-        // Puedes añadir un constructor y métodos auxiliares si lo consideras necesario
 
         public Tarea(int tareaID, string descripcion, int durac)
         {
